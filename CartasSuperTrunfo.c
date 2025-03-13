da324fca48d5455d587def2abe255091486ff88a
Rostauskas <kaue.tal@gmail.com>
2025-03-13 15:13:32 -0300

    Subir arquivos atividade

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    char estado;
+    char codigo [3]; 
+    char cidade [10];
+    int populacao;
+    float area; 
+    float pib;
+    int qtdPontosTuristicos;
+
+    printf("Informe o estado da carta 1: \n");
+    scanf("%c", &estado); 
+
+    printf("Informe o codigo da carta 1: \n");
+    scanf("%3s", codigo); 
+
+    printf("Informe a cidade da carta 1: \n");
+    scanf(" %[^\n]", cidade); // Lê a linha inteira com espaços
+    
+    printf("Carta 1:\n");
+    printf("Estado informado: %c\n", estado);
+    printf("Codigo informado: %s\n", codigo);            
+    printf("Cidade informada: %s\n", cidade);            
+
+    return 0;
+}
